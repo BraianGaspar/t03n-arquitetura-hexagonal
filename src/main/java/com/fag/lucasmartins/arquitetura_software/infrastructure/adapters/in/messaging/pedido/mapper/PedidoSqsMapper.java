8ce8622e7a47ca8a088221c792b069caf9f9ac28
BraianGaspar <braian_gaspar@hotmail.com>
2026-05-25 22:28:32 -0300

    Configurações do properties

--- src/main/java/com/fag/lucasmartins/arquitetura_software/infrastructure/adapters/in/messaging/pedido/mapper/PedidoSqsMapper.java
+++ src/main/java/com/fag/lucasmartins/arquitetura_software/infrastructure/adapters/in/messaging/pedido/mapper/PedidoSqsMapper.java
@@ -1,10 +1,10 @@
 package com.fag.lucasmartins.arquitetura_software.infrastructure.adapters.in.messaging.pedido.mapper;
 
-import com.fag.lucasmartins.arquitetura_software.application.ports.in.service.PedidoServicePort;
 import com.fag.lucasmartins.arquitetura_software.core.domain.bo.*;
 import com.fag.lucasmartins.arquitetura_software.infrastructure.adapters.in.messaging.pedido.dto.PedidoSqsEventDTO;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,18 +16,22 @@ public class PedidoSqsMapper {
         PessoaBO pessoa = new PessoaBO();
         pessoa.setId(dto.getCustomerId());
 
-        List<PedidoProdutoBO> itens = dto.getOrderItems()
-                .stream()
-                .map(item -> {
-                    ProdutoBO produto = new ProdutoBO();
-                    produto.setId(item.getSku());
+        List<PedidoProdutoBO> itens = new ArrayList<>();
+        
+        if (dto.getOrderItems() != null && !dto.getOrderItems().isEmpty()) {
+            itens = dto.getOrderItems()
+                    .stream()
+                    .map(item -> {
+                        ProdutoBO produto = new ProdutoBO();
+                        produto.setId(item.getSku());
 
-                    PedidoProdutoBO pp = new PedidoProdutoBO();
-                    pp.setProduto(produto);
-                    pp.setQuantidade(item.getAmount());
-                    return pp;
-                })
-                .collect(Collectors.toList());
+                        PedidoProdutoBO pp = new PedidoProdutoBO();
+                        pp.setProduto(produto);
+                        pp.setQuantidade(item.getAmount());
+                        return pp;
+                    })
+                    .collect(Collectors.toList());
+        }
 
         PedidoBO pedido = new PedidoBO();
         pedido.setPessoa(pessoa);
